97e30c88a40f6f11331662a979332ea7416f9473
Christer Gundersen <46853403+christer-io@users.noreply.github.com>
2023-08-19 20:19:45 +0200

    Added errorhandling

--- app/myspot/[id]/page.tsx
+++ app/myspot/[id]/page.tsx
@@ -1,6 +1,6 @@
-import HeaderYr from "@/components/HeaderYr"
-import ListWeather from "@/components/ListWeather"
-
+import HeaderYr from "@/components/HeaderYr";
+import ListWeather from "@/components/ListWeather";
+import Image from "next/image";
 
 
 type PageProps = {
@@ -26,20 +26,30 @@ async function MySpotPage({params: {id}}: PageProps) {
 
   let feedback = "not working";
   if(mySpot.status == "ZERO_RESULTS") {
-    feedback="Not working...."
-     
+    feedback=`Oops, we could not find ${id}....`
+    return (
+      <div className="flex flex-col text-center items-center">
+          <div className="flex-1"><HeaderYr /></div>
+          
+          <div>{feedback}</div>
+          <div className="items-center">
+            <Image className="object-contain" src="/404.svg" width={600} height={600} alt="404 illustration" />
+          </div>
+      </div>
+    )    
   } else {
      feedback = "Showing results for:"
+     return (
+    
+      <div className="text-center">
+      <HeaderYr />
+          <div>{feedback} {mySpot?.results[0]?.formatted_address} </div>
+          <ListWeather status={mySpot.status} lat={mySpot?.results[0]?.geometry.location.lat} lng={mySpot?.results[0]?.geometry.location.lng} />
+      </div>
+    )
   }
   
-  return (
-    <div className="text-center">
-    <HeaderYr />
-        <div>{feedback} {mySpot?.results[0]?.formatted_address} </div>
-        <ListWeather status={mySpot.status} lat={mySpot?.results[0]?.geometry.location.lat} lng={mySpot?.results[0]?.geometry.location.lng} />
-           
-    </div>
-  )
+  
 }
 
 export default MySpotPage
--- components/SearchButton.js
+++ components/SearchButton.js
@@ -1,6 +1,6 @@
 "use client"
 import { experimental_useFormStatus as useFormStatus } from "react-dom";
-
+import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
 
 
 function SearchButton() {
@@ -9,7 +9,7 @@ function SearchButton() {
   return (
     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-4 rounded-full disabled:opacity-50">
       {pending && "Searching..."}    
-      {!pending && "Search" }
+      {!pending && <MagnifyingGlassIcon className="h-5 w-5"/> }
     </button>
   )
 }
